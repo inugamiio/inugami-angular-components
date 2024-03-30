e395cabf5225ac6a3489201deafae5e875f03959
Patrick Guillerm <pguillerm@smartwavesa.com>
2024-03-30 23:15:21 +0100

    [#1] FEAT: implement timeline component

--- icon-extractor.js
+++ icon-extractor.js
@@ -4,7 +4,7 @@ const JSDOM = require('jsdom');
 const FILES = [
     {
         path: 'src/assets/inu-icons.svg',
-        svgPrefix: '<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">',
+        svgPrefix: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">',
         svgName: 'INU_ICON_SVG',
         enumName: 'INU_ICON_TYPE',
         iconSetName: 'INU_ICON',
